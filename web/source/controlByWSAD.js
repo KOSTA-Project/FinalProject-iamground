6342852b0aba0dc52b3869590ab493281a521b6f
JIHUN KANG <84777431+Ji-Hun-Kang@users.noreply.github.com>
2021-06-25 23:11:25 +0900

    210625 update kosta final project
    - javascript

--- web/source/controlByWSAD.js
+++ web/source/controlByWSAD.js
@@ -1,4 +1,4 @@
-/ start with DOM loading finish
+// start with DOM loading finish
 document.addEventListener("DOMContentLoaded", function(){
     // get element object by id
     var log = document.querySelector('#log');
@@ -6,57 +6,81 @@ document.addEventListener("DOMContentLoaded", function(){
 
     // set keypress event and function
     document.addEventListener('keypress', sendKeyToPhp);
-    document.addEventListener('keyup', sendXToPhp);      
+    document.addEventListener('keyup', sendXToPhp);            
 
-    function isWASD(e){
-        var wasdCheck = true;
-        
-        if(e.key!=='w' && e.key!=='s' && e.key!=='a' && e.key!=='d'){
-            alert("please press key between 'w', 's', 'a', 'd'");
-            wasdCheck = false;            
-        } else {
-            wasdCheck = true;
-        }    
-        return wasdCheck;
-    }
-
-    function sendWithAJAX(key){
-        var httpRequest = new XMLHttpRequest();
-
-        httpRequest.onreadystatechange = function(){
-            log.textContent = "called onreadystatechange..   ";
-
-            if(httpRequest.readyState==4 && httpRequest.status == 200){
-                // get response data to server (is it null now)
-                // var response = JSON.parse(httpRequest.responseText);
-                log.textContent = "press button"+httpRequest.responseText;                
-            } 
-        };
-
-        httpRequest.open('POST', './controller/NetworkController.php');
-        httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        httpRequest.send('direction=' + key+ '&address='+'192.168.0.154');
-    }
-
-    // when key wsad pressed send key word to MO
-    function sendKeyToPhp(e){
-        
-        if(isWASD(e)===false){
-            return;
-        }      
-        
-        sendWithAJAX(e.key);        
-    }
-
-    // when key wsad up send key x to MO
-    function sendXToPhp(e){
-        if(isWASD(e)===false){
-            return;
-        }  
-
-        sendWithAJAX('x');
-    }
+    
 
     
 
 });
+
+function isWASD(e){
+    var wasdCheck = true;
+    
+    if(e.key!=='w' && e.key!=='s' && e.key!=='a' && e.key!=='d'){
+        alert("please press key between 'w', 's', 'a', 'd'");
+        wasdCheck = false;            
+    } else {
+        wasdCheck = true;
+    }    
+    return wasdCheck;
+}
+
+function sendWithAJAX(key){
+    var httpRequest = new XMLHttpRequest();
+
+    httpRequest.onreadystatechange = function(){
+        log.textContent = "called onreadystatechange..   ";
+
+        if(httpRequest.readyState==4 && httpRequest.status == 200){
+            // get response data to server (is it null now)
+            // var response = JSON.parse(httpRequest.responseText);
+            log.textContent = "press button"+httpRequest.responseText;                
+        } 
+    };
+
+    httpRequest.open('POST', './controller/NetworkController.php');
+    httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    httpRequest.send('direction=' + key+ '&address='+'192.168.0.154');
+}    
+
+// when key wsad pressed send key word to MO
+function sendKeyToPhp(e){
+    if(isWASD(e)===false){
+        return;
+    }      
+    
+    sendWithAJAX(e.key);
+    changeBtnColor(e.key, 'o');        
+}
+
+// when key wsad up send key x to MO
+function sendXToPhp(e){
+    if(isWASD(e)===false){
+        return;
+    }  
+
+    sendWithAJAX('x');
+    changeBtnColor(e.key, 'x');
+}
+
+function changeBtnColor(e, ox){
+    if(ox === 'x'){
+        var btnColor = "grey"; 
+    } else if(ox === 'o'){
+        var btnColor = "red";
+    }
+    document.getElementById(e).style.backgroundColor = btnColor;      
+}
+
+function sendKeyByMouse(btnValue){
+    sendWithAJAX(btnValue);
+    changeBtnColor(btnValue, 'o');
+}
+
+function sendXByMouse(btnValue){
+    sendWithAJAX('x');
+    changeBtnColor(btnValue, 'x');
+
+}
+
