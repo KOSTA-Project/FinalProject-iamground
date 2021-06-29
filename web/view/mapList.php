49dfd1586c08aa81722e3c910e89dc30bd0dfb5a
JIHUN KANG <84777431+Ji-Hun-Kang@users.noreply.github.com>
2021-06-29 17:27:16 +0900

    210629 update kosta final project
    - mapList.php

--- web/view/mapList.php
+++ web/view/mapList.php
@@ -1,45 +1,54 @@
 <?php
     srand((double)microtime()*1000000);
-
-    //$imageArr = array("warehouse1.jpg", "warehouse2.jpg", "restaurant1.jpg");
-    //$random = time()%count($imageArr);
+    header("Cache-Control: no cache");
 ?>
 <!DOCTYPE html>
 <html>
   <head>
+    <META http-equiv=Expires content=-1>
+    <META http-equiv=Pragma content=no-cache>
+    <META http-equiv=Cache-Control content=No-Cache>
     <meta charset="utf-8">
     <title>mapList</title>
     <link rel="stylesheet" href="./source/iamground.css">
-        <?php
-        if(session_status() != PHP_SESSION_ACTIVE){
-         session_start();
-            }
-            if(isset($_SESSION['userId'])){
-            if($this->action=='login'){
-                print "<script>alert('".$this->data."');</script>";
-            }
-            print $_SESSION['userId']." login ing...<br>";
-            } else {
-                header("Location:view/loginForm.php");
-            }
+    <script src="./source/iamground.js"></script>
+	<?php
+    	if(session_status() != PHP_SESSION_ACTIVE){
+       	 session_start();
+        // header(Pragma: no-cache);
+        // header(Cache-Control: no-cache,must-revalidate);
+   	    }
+  	    if(isset($_SESSION['userId'])){
+     	    if($this->action=='login'){
+       	        print "<script>alert('".$this->data."');</script>";
+      	    }
+     	    print $_SESSION['userId']." login ing...<br>";
+   	    } else {
+   	        header("Location:view/loginForm.php");
+   	    }
         $mapLists = $this->responseData;
-        //var_dump($this->responseData);
-        ?>
+        //var_dump($mapLists);
+	?>
+    
     </head>
     <body>
-        <div class='listForm'>
+        <div class='listForm'>            
             <div class='list'>
+            
             <? for($i = 0; $i < count($mapLists); $i++) : ?>
-                <form method="POST" action="../am/index.php?action=moList">
-                <input type="hidden" name="mId" value="<? echo $mapLists[$i]->getMapId() ?>"/>
-                <button class='map' style="background-image: url('./source/image/<?=rand(1, 3)?>.jpg')">
+                <button class='map' style="background-image: url('./source/image/<?=rand(1, 3)?>.jpg')" onclick="getMoList('<? echo $mapLists[$i]->getMapId() ?>', <? echo $i?>)">
                     <div class='location'><? echo $mapLists[$i]->getMapLocation(); ?></div>
-                    <div class='mapId'>map id : <? echo $mapLists[$i]->getMapId(); ?></div>
+                    <div class='mapId'>map id : <? echo $mapLists[$i]->getMapId(); ?></div> 
                 </button>
-                </form>
+                <div id="moList<?echo $i?>">
+                
+                </div>
+                                
             <? endfor; ?>
+
             </div>
         </div>
-
+        
 </body>
 </html>
+ 
