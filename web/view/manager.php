b848dc248bef6135a81127509b3f6d92242d6bdf
JIHUN KANG <84777431+Ji-Hun-Kang@users.noreply.github.com>
2021-06-29 17:35:58 +0900

    210629 update kosta final project
    - manager.php

--- web/view/manager.php
+++ web/view/manager.php
@@ -1,29 +1,34 @@
 <!DOCTYPE html>
 <html>
+<?php
+header("Cache-Control: no cache");
+?>
   <head>
     <meta charset="utf-8">
     <title>Manager</title>
     <link rel="stylesheet" href="./source/iamground.css">
-        <?php
-
-        ?>
-    </head>
-    <body>
-                <div class='container'>
-                    <div>
-                        <span><? echo '회원 관리 페이지'; ?></span>
-                       <form method="POST" action="./index.php?action=joinform">
-                           <br> <input type="submit" value ="User 추가"/> <br>
-                       </form>
-
-                        <form method="POST" action="../am/index.php?action=moList">
-                          <tr>
-                              <td>User ID :</td>
-                              <td><input type="text" name="uid"/></td>
-                          </tr>
-                              <input type="submit" value ="User 조회"/>
-                        </form>
-                    </div>
-                 </div>
-</body>
+  </head>
+  <body>
+    <div class='container'>
+      <h3>회원 관리</h3>
+      <form method="POST" action="../am/index.php?action=userInfo">
+        <table class="manageTable">
+          <tr><td>User Id</td></tr>
+        <tr>          
+          <td><input type="text" name="userId"/></td>
+        </tr>
+        <tr>
+          <td><input type="submit" class="btnManage" value ="Search"/></td>
+        </tr>
+        </table>
+      </form>     
+      <form method="POST" action="./index.php?action=joinForm">
+        <table class="manageTable">
+        <tr>
+          <td><input type="submit" class="btnManage" value ="User 추가"/> </td>
+        </tr>
+        </table>
+      </form>
+    </div>
+  </body>
 </html>
