1650da5f2ac59a7ce88dc4daa93efce027852854
JIHUN KANG <84777431+Ji-Hun-Kang@users.noreply.github.com>
2021-06-30 17:11:10 +0900

    210630 kosta final project
    - mo dao

--- web/model/MoDAO.php
+++ web/model/MoDAO.php
@@ -1,69 +1,75 @@
 <?php
-
+    //echo "<script> console.log('modao load'); </script>";
     require_once 'MoDTO.php';
     require_once 'DBConnector.php';
-
+      
     class MoDAO{
         //private static $instance = new UserDAO();
         private static $instance = null;
-        private $connection = null;
+        private $connection = null;        
 
-        private function __construct(){
-            $dbConnector = DBConnector::getInstance();
-            $this->connection = $dbConnector->getConnection();
+        private function __construct(){                        
+            $dbConnector = DBConnector::getInstance();                                  
+            $this->connection = $dbConnector->getConnection();            
         }
 
+        //
         public static function getInstance(){
-            echo "<script> console.log('modao getInstance'); </script>";
             if(self::$instance == null){
                 self::$instance = new self;
-            }
+            } 
             return self::$instance;
-
-        }
-
+                     
+        }        
+        
         public function mSelectMoByMapId($mapId){
-            echo "<script> console.log('mo controller mSelectMoByMapId()'); </script>";
-
             $moDTOs = array();
             $sql = "SELECT * FROM mobile_object WHERE map_id=?";
-            //$sql = "SELECT * FROM mobile_object";
             $stmt = $this->connection->prepare($sql);
-
-            $stmt->bind_param('s', $mapId);
-            $stmt->execute();
-
-
+            $stmt->bind_param('s', $mapId);           
+            $stmt->execute();            
             $stmt->bind_result($moId, $userId, $mId, $type);
-
             while($stmt->fetch()){
                 $moDTOs[] = new MoDTO($moId, $userId, $mId, $type);
             }
-
             return $moDTOs;
         }
-        public function insertByMo(){
-                $sql = "insert into mobile_object values(?,?)";
-                $stm = $this->conn->prepare ($sql);
-                $stm->bindValue (1, $this->getMoId ());
-                $stm->bindValue (2, $this->getMoType ());
-                $stm->execute();
+
+        public function mSelectMoByUserId($userId){
+            $moDTOs = array();
+            $sql = "SELECT * FROM mobile_object WHERE user_id=?";
+            $stmt = $this->connection->prepare($sql);
+            $stmt->bind_param('s', $userId);           
+            $stmt->execute();            
+            $stmt->bind_result($moId, $userId, $mId, $type);
+            while($stmt->fetch()){
+                $moDTOs[] = new MoDTO($moId, $userId, $mId, $type);
+            }
+            return $moDTOs;
         }
 
-        public function updateByMo(){
-                $sql = "update mobile_object set mo_id=?, mo_type=? where user_id=?;";
-                $stm = $this->conn->prepare ($sql);
-                $stm->bindValue (1, $m->getMoId ());
-                $stm->bindValue (2, $m->getMoType ());
-                $stm->bindValue (3, $m->getUserId ());
-                $stm->execute();
-        }
-        public function deleteByMo($userId){
-                $sql = "delete from mobile_object where user_id=?;";
-                $stm = $this->conn->prepare ($sql);
-                $stm->bindValue (1, $userId);
-                $stm->execute();
+        public function insertMo($moDTO){
+
+            $sql = "INSERT INTO mobile_object VALUES (?, ?, ? ,?)";
+            $stm = $this->connection->prepare($sql);
+            $stm->bind_param('ssss', $moDTO->getMoId(), $moDTO->getUserId() ,$moDTO->getMapId(), $moDTO->getMoType());
+            $stm->execute();
         }
 
-    }
+        public function mUpdateMo($moDTO){
+            echo "<script> console.log('update start'); </script>";
+            $sql = "UPDATE mobile_object SET mo_type=? WHERE mo_id=?";
+            $stm = $this->connection->prepare ($sql);
+            $stm->bind_param('ss', $moDTO->getMoType(), $moDTO->getMoId() );
+            $stm->execute();
+        }
+
+        public function mDeleteMo($moId){
+            $sql = "delete from mobile_object where mo_id=?;";
+            $stm = $this->connection->prepare ($sql);
+            $stm->bind_param ('s',$moId);
+            $stm->execute();
+        }
+
+      }
 ?>
