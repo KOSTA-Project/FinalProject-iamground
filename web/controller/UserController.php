b1ea9a9768754485f4146d168397bf4c5145a0b9
will5810 <79901583+will5810@users.noreply.github.com>
2021-06-30 17:32:19 +0900

    Update UserController.php

--- web/controller/UserController.php
+++ web/controller/UserController.php
@@ -7,61 +7,61 @@
         private $userdao;
         public function __construct(){
             echo "<script> console.log('usercontroller construct'); </script>";
-            $this->userdao = UserDAO::getInstance();                                   
+            $this->userdao = UserDAO::getInstance();
         }
 
         public function returnView($responseData=null){
             $frontController = new FrontController();
             $frontController->controlView($this->view, $responseData);
         }
-        
+
         public function login($userId, $userPw){
             echo "<script> console.log('user controller login()'); </script>";
-            
-            $userDTO = $this->userdao->selectById($userId);            
+
+            $userDTO = $this->userdao->selectById($userId);
             if($userDTO==null){                                     // not exist id
                 $this->view = 'loginForm.php';                      // check ///////////////////////////
                 $this->returnView();
             } else {
-                if($userDTO->getUserPw()==$userPw){                 // login ok              
+                if($userDTO->getUserPw()==$userPw){                 // login ok
                     session_start();
                     $_SESSION['userId']=$userDTO->getUserId();
                     $_SESSION['userType']=$userDTO->getUserType();
                     if($_SESSION['userType']=='4'){                 // manager login
                     $frontController = new FrontController('action=manager');
                     $frontController->run();
-                    } else {                                        // user login 
+                    } else {                                        // user login
                     $frontController = new FrontController('action=mapList');
                     $frontController->run();
                     }
-                } else {                                            // wrong password                    
+                } else {                                            // wrong password
                     $this->view = 'loginForm.php';                  // check ///////////////////////////
                     $this->returnView();
                 }
             }
-            
-        }
-        public function logout($userId, $userPw){
-            echo "<script> console.log('function logout()'); </script>";
- 	session_start();
-	session_destroy();
 
-            $this->view = 'loginForm.php';                   
+        }
+        public function logout(){
+            echo "<script> console.log('function logout()'); </script>";
+            session_start();
+            session_unset();
+            session_destroy();
+            $this->view = 'loginForm.php';
             $this->returnView();
         }
 
         public function join(){
-      	    echo "<script> console.log('user controller join()'); </script>";
+            echo "<script> console.log('user controller join()'); </script>";
             $this->userdao->insertUser($_POST['userId'],$_POST['userPw'],$_POST['userType']);
             $frontController = new FrontController('action=userInfo');       // check ///////////////////////////
-            $frontController->run();	
+            $frontController->run();
         }
-        
-        
+
+
         public function userInfo($userId){
             echo "<script> console.log('function userinfo()'); </script>";
-                      
-           
+
+
             $mapdao = MapDAO::getInstance();
             $modao = MoDAO::getInstance();
             $dtoArr=array();
@@ -70,17 +70,17 @@
 
             $userDTO = $this->userdao->selectById($userId);
             array_push($dtoArr, $userDTO);
-                     
+
             $mapDTOs = $mapdao->mSelectMapByUserId($userId);   // mapList
             array_push($dtoArr, $mapDTOs);
-    
+
             for($i=0; $i<count($mapDTOs); $i++){
                 $mapId = $mapDTOs[$i]->getMapId();    // mapList 안에 mapId
                 $moDTOs = $modao->mSelectMoByMapId($mapId);
-                array_push($modtoArr, $moDTOs);	
+                array_push($modtoArr, $moDTOs);
             }
             array_push($dtoArr, $modtoArr);
-            
+
             if($dtoArr[0]==null){                          // user id not exist
                 $this->view = 'manager.php';            // check ///////////////////////////
                 $this->returnView();
@@ -89,17 +89,17 @@
                 $this->view = 'editForm.php';
                 $this->returnView($dtoArr);
             }
-       }       
-       
+       }
+
 
         public function cUpdateUser($userId, $userPw, $userType){                         // check ///////////////////////////
              echo "<script> console.log('function cupdateuser()'); </script>";
-            $userDTO = new UserDTO($userId, $userPw, $userType);                        
+            $userDTO = new UserDTO($userId, $userPw, $userType);
             $this->userdao->mUpdateUser($userDTO);
             $frontController = new FrontController('action=userInfo');
             $frontController->run();
         }
-        
+
         public function cDeleteUser($userId){
             echo "<script> console.log('function cDeleteMo'); </script>";
             $this->userdao->mDeleteUser($userId);
@@ -108,5 +108,5 @@
         }
     }
 
-    
+
 ?>
