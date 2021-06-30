7545517c6c6cecdf9865a9b6b3add9ae57edd187
JIHUN KANG <84777431+Ji-Hun-Kang@users.noreply.github.com>
2021-06-30 17:12:12 +0900

    210630 update kosta final project
    - user dao

--- web/model/UserDAO.php
+++ web/model/UserDAO.php
@@ -1,6 +1,9 @@
 
 <?php
+    //echo "<script> console.log('userdao load'); </script>";
     require_once 'UserDTO.php';
+    require_once 'MapDTO.php';
+    require_once 'MoDTO.php';
     require_once 'DBConnector.php';
       
     class UserDAO{
@@ -15,7 +18,6 @@
         }
 
         public static function getInstance(){
-            echo "<script> console.log('dao getInstance'); </script>";
             if(self::$instance == null){
                 self::$instance = new self;
             } 
@@ -27,43 +29,56 @@
             $userDTO = null;
             $sql = "SELECT * FROM user WHERE user_id=?";
             $stmt = $this->connection->prepare($sql);
-            
             $stmt->bind_param('s', $userId);           
             $stmt->execute();
-            
             $stmt->bind_result($id, $pw, $type);
             if($stmt->fetch()){
                 $userDTO = new UserDTO($id, $pw, $type);
             }
-            
             return $userDTO;
         }
 
-	public function insertByUser($m){
-		$sql = "insert into user values(?,?,?)";
-		$stm = $this->connection->prepare($sql);
-                echo "<script> console.log('11'); </script>";
-		$stm->bind_param('sss',$_POST['juid'],$_POST['jpwd'],$_POST['jtype']);
-                echo "<script> console.log('22'); </script>";
-		$stm->execute();
-	}
+        public function mSelectAllByUserId($userId){
+            $dtoArr = array();
+            $sql = "SELECT u.user_id, u.user_pwd, u.user_type, m.map_id, m.map_location , mo.mo_id , mo.mo_type
+         	FROM user u INNER JOIN map m ON m.user_id = u.user_id LEFT OUTER JOIN mobile_object mo ON mo.user_id = u.user_id
+         	WHERE u.user_id = ?";
+            $stmt = $this->connection->prepare($sql);
+            $stmt->bind_param('s', $userId);
+            $stmt->execute();
+            $stmt->bind_result($uid,$upwd, $utype, $mid, $mlocation, $moid, $motype);
+            while($stmt->fetch()){
+                $userDTO = new UserDTO($uid,$upwd,$utype);
+                $mapDTO = new MapDTO($mid , null, $mlocation);
+                $moDTO = new MoDTO($moid, null, null, $motype);
+	            array_push($dtoArr, $userDTO);
+                array_push($dtoArr, $mapDTO);
+                array_push($dtoArr, $moDTO);
+            }
+            return $dtoArr;
+        }
 
-	public function updateByUser(){
-                echo "<script> console.log('update start'); </script>";
-		$sql = "update user set user_id=?, user_pwd=?, user_type=? where user_id=?";		
-		$stm = $this->connection->prepare ($sql);
-		$stm->bind_param('ssss',$_POST['Uuid'],$_POST['Upwd'],$_POST['Utype'],$_POST['Uuid']);
-		$stm->execute();									
-	}
-	public function deleteByUser($userId){
-        echo $userId;
-                echo "<script> console.log('deleteByUser'); </script>";
-		$sql = "delete from user where user_id=?;";
-		$stm = $this->connection->prepare ($sql);
-		$stm->bind_param ('s',$userId);
-		$stm->execute();
-	}
-
+        public function insertUser($m){
+            $sql = "INSERT INTO user VALUES(?,?,?)";
+            $stm = $this->connection->prepare($sql);
+            $stm->bind_param('sss',$_POST['userId'],$_POST['userPw'],$_POST['userType']);
+            $stm->execute();
+        }
+        
+        public function mUpdateUser($userDTO){
+            echo "<script> console.log('update start'); </script>";
+            $sql = "update user set  user_pwd=?, user_type=? where user_id=?";		
+            $stm = $this->connection->prepare ($sql);
+            $stm->bind_param('sss', $userDTO->getUserPw(), $userDTO->getUserType(), $userDTO->getUserId());
+            $stm->execute();									
+        }
+        
+        public function mDeleteUser($userId){
+            $sql = "delete from user where user_id=?;";
+            $stm = $this->connection->prepare ($sql);
+            $stm->bind_param ('s',$userId);
+            $stm->execute();
+        }
 
     }
 ?>
