8bba12570a2af4ecf01e0aa2a6b09c748dee7975
will5810 <79901583+will5810@users.noreply.github.com>
2021-07-01 19:08:23 +0900

    Update MoDAO.php

--- web/model/MoDAO.php
+++ web/model/MoDAO.php
@@ -2,35 +2,35 @@
     //echo "<script> console.log('modao load'); </script>";
     require_once 'MoDTO.php';
     require_once 'DBConnector.php';
-      
+
     class MoDAO{
         //private static $instance = new UserDAO();
         private static $instance = null;
-        private $connection = null;        
+        private $connection = null;
 
-        private function __construct(){                        
-            $dbConnector = DBConnector::getInstance();                                  
-            $this->connection = $dbConnector->getConnection();            
+        private function __construct(){
+            $dbConnector = DBConnector::getInstance();
+            $this->connection = $dbConnector->getConnection();
         }
 
         //
         public static function getInstance(){
             if(self::$instance == null){
                 self::$instance = new self;
-            } 
+            }
             return self::$instance;
-                     
-        }        
-        
+
+        }
+
         public function mSelectMoByMapId($mapId){
             $moDTOs = array();
             $sql = "SELECT * FROM mobile_object WHERE map_id=?";
             $stmt = $this->connection->prepare($sql);
-            $stmt->bind_param('s', $mapId);           
-            $stmt->execute();            
-            $stmt->bind_result($moId, $userId, $mId, $type);
+            $stmt->bind_param('s', $mapId);
+            $stmt->execute();
+            $stmt->bind_result($moId, $userId, $mId, $type,$status);
             while($stmt->fetch()){
-                $moDTOs[] = new MoDTO($moId, $userId, $mId, $type);
+                $moDTOs[] = new MoDTO($moId, $userId, $mId, $type ,$status);
             }
             return $moDTOs;
         }
@@ -39,28 +39,28 @@
             $moDTOs = array();
             $sql = "SELECT * FROM mobile_object WHERE user_id=?";
             $stmt = $this->connection->prepare($sql);
-            $stmt->bind_param('s', $userId);           
-            $stmt->execute();            
-            $stmt->bind_result($moId, $userId, $mId, $type);
+            $stmt->bind_param('s', $userId);
+            $stmt->execute();
+            $stmt->bind_result($moId, $userId, $mId, $type, $status);
             while($stmt->fetch()){
-                $moDTOs[] = new MoDTO($moId, $userId, $mId, $type);
+                $moDTOs[] = new MoDTO($moId, $userId, $mId, $type ,$status);
             }
             return $moDTOs;
         }
 
         public function insertMo($moDTO){
 
-            $sql = "INSERT INTO mobile_object VALUES (?, ?, ? ,?)";
+            $sql = "INSERT INTO mobile_object VALUES (?, ?, ? ,? ,?)";
             $stm = $this->connection->prepare($sql);
-            $stm->bind_param('ssss', $moDTO->getMoId(), $moDTO->getUserId() ,$moDTO->getMapId(), $moDTO->getMoType());
+            $stm->bind_param('sssss', $moDTO->getMoId(), $moDTO->getUserId() ,$moDTO->getMapId(), $moDTO->getMoType() ,$moDTO->getMoStatus());
             $stm->execute();
         }
 
         public function mUpdateMo($moDTO){
             echo "<script> console.log('update start'); </script>";
-            $sql = "UPDATE mobile_object SET mo_type=? WHERE mo_id=?";
+            $sql = "UPDATE mobile_object SET mo_type=? ,mo_status=? WHERE mo_id=?";
             $stm = $this->connection->prepare ($sql);
-            $stm->bind_param('ss', $moDTO->getMoType(), $moDTO->getMoId() );
+            $stm->bind_param('sss', $moDTO->getMoType(),$moDTO->getMoStatus(), $moDTO->getMoId() );
             $stm->execute();
         }
 
