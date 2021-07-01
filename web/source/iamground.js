26e5b135d70a3c1a70cc0477b91acc42a49b8436
JIHUN KANG <84777431+Ji-Hun-Kang@users.noreply.github.com>
2021-07-01 19:00:35 +0900

    210701 update kosta final project
    - iamground.js

--- web/source/iamground.js
+++ web/source/iamground.js
@@ -2,26 +2,39 @@ function getMoList(e, index){
     var moListDiv = document.querySelector('#moList'+index);
     var response = "";
     var responseArr;
+    var splitedVal = [];
     var html = "";
     var httpRequest = new XMLHttpRequest();
     
     httpRequest.onreadystatechange = function(){
         if(httpRequest.readyState==4 && httpRequest.status==200){
             response = httpRequest.responseText;
-            // check //////////////////
-            responseArr = response.split('/');
+            responseArr = response.split("/");
             
+            if(responseArr.length == 1){
+                alert("not exist mobile object");
+            }
+
             var i;
-            for(i = 0; i < responseArr.length; i++){
-            html += "<form class='mo' method='POST' action='../am/index.php?action=monitoring'>";
-            html += "<button class='btnMo' type='submit'>";
-            html += "<input type='hidden' name='moId' value='"+responseArr[i]+"'/>";
-            html += responseArr[i];
-            html += "</button>";
-            html += "</form>"
+            for(i = 1; i < responseArr.length; i++){
+                splitedVal[i-1] = responseArr[i].split("|");                
+            }
             
-        }
-        moListDiv.innerHTML = html;
+            for(i = 0; i < splitedVal.length; i++){
+                html += "<form class='mo' method='POST' action='../am/index.php?action=monitoring'>";
+                html += "<button type='submit'>";
+                html += "<input type='hidden' name='moId' value='"+splitedVal[i][0]+"'/>";
+                html += splitedVal[i][0];
+                html += "</button>";
+                if(splitedVal[i][1] === '0'){
+                    html += "<button class='btnMo' type='button' style='background-color: green'></button>";
+                } else {
+                    
+                    html += "<button class='btnMo' type='button' style='background-color: red'></button>";
+                }
+                html += "</form>";
+            }            
+            moListDiv.innerHTML = html;
         }   
     }
     httpRequest.open('POST', './index.php?action=moList');
