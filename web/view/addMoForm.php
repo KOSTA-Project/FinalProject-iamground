855453395cd5e0e590f37896fa9bf54349e64f0c
will5810 <79901583+will5810@users.noreply.github.com>
2021-07-01 19:12:48 +0900

    Update addMoForm.php

--- web/view/addMoForm.php
+++ web/view/addMoForm.php
@@ -3,25 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <link rel="stylesheet" href="./source/iamground.css">
 </head>
 <body>
+<div class="container">
     <h3>Add Mo</h3>
     <form method="POST" action="../am/index.php?action=addMo">
     <input type="hidden" name="userId" value="<? echo $_POST['userId']?>"/>
     <input type="hidden" name="mapId" value="<? echo $_POST['mapId']?>"/>
-    <table>
+    <table class="manageTable">
         <tr>
             <td>mo id:</td>
-            <td><input type="text" name="moId"/></td>
+            <td><input type="text" name="moId" autocomplete="off"/></td>
         </tr>
         <tr>
             <td>mo type:</td>
-            <td><input type="text" name="moType"/></td>
+            <td><input type="text" name="moType" autocomplete="off"/></td>
         </tr>
         <tr>
-            <td><input type="submit" value="Finish"/></td> 
-        </tr>        
+            <td>mo Status:</td>
+            <td><input type="text" name="moStatus" autocomplete="off"/></td>
+        </tr>
+        <tr>
+            <td><input type="submit" value="추가"/></td>
+        </tr>
     </table>
     </form>
+</div>
 </body>
 </html>
