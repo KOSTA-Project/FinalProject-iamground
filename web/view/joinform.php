a46c021b468d4ab0b0cf59c073e5063f14d8737d
will5810 <79901583+will5810@users.noreply.github.com>
2021-07-01 19:10:41 +0900

    Update joinform.php

--- web/view/joinform.php
+++ web/view/joinform.php
@@ -4,30 +4,32 @@
     <meta charset="utf-8">
     <title>Manager</title>
     <link rel="stylesheet" href="./source/iamground.css">
-        <?php
-
-        ?>
     </head>
     <body>
-                <div class='container'>
-                    <div>
-                        <span><? echo '회원 생성'; ?></span>
-                        <form method="POST" action="./index.php?action=join">
-                          <tr>
-                              <br><td>User ID :</td>
-                              <td><input type="text" name="juid"/></td>
-                          </tr>
-                          <tr>
-                              <br><td>User PWD :</td>
-                              <td><input type="text" name="jpwd"/></td>
-                          </tr>
-                          <tr>
-                              <br><td>User Type :</td>
-                              <td><input type="text" name="jtype"/></td>
-                          </tr>
-                              <br><input type="submit" value ="확인"/>
-                        </form>
-                    </div>
-                 </div>
-</body>
+        <div class='container'>
+            <div>
+                <h3>회원 생성</h3>
+                 <form method="POST" action="./index.php?action=join">
+                    <table class="manageTable">
+                        <tr>
+                            <td>아이디 </td> <td><input type="text" name="userId" autocomplete="off" /></td>
+                        </tr>
+                        <tr>
+                            <td>비밀번호 </td> <td><input type="text" name="userPw" autocomplete="off"/></td>
+                        </tr>
+                        <tr>
+                            <td>권한 </td> <td><input type="text" name="userType" autocomplete="off"/></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2"><input type="submit" class="btnManage" value ="확인"/></td>
+                        </tr>
+                    </table>
+                </form>
+                    <form method="POST" action="../am/index.php?action=manager">
+                    <input type="submit" class="btnManage" value ="돌아가기"/>
+                </form>
+
+            </div>
+         </div>
+    </body>
 </html>
