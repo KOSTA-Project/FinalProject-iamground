aa626bed30851c6b9a59faab38d35cf80ac2944c
will5810 <79901583+will5810@users.noreply.github.com>
2021-07-01 19:12:26 +0900

    Update addMapForm.php

--- web/view/addMapForm.php
+++ web/view/addMapForm.php
@@ -3,24 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <link rel="stylesheet" href="./source/iamground.css">
 </head>
 <body>
+    <div class='container'>
     <h3>Add Map</h3>
     <form method="POST" action="../am/index.php?action=addMap">
     <input type="hidden" name="userId" value="<? echo $_POST['userId']?>"/>
-    <table>
+    <table class="manageTable">
         <tr>
             <td>map id:</td>
-            <td><input type="text" name="mapId"/></td>
+            <td><input type="text" name="mapId" autocomplete="off"/></td>
         </tr>
         <tr>
             <td>map Location:</td>
-            <td><input type="text" name="mapLocation"/></td>
+            <td><input type="text" name="mapLocation" autocomplete="off"/></td>
         </tr>
         <tr>
-            <td><input type="submit" value="Finish"/></td> 
-        </tr>        
+            <td><input type="submit" value="추가" autocomplete="off"/></td>
+        </tr>
     </table>
     </form>
+    </div>
 </body>
 </html>
