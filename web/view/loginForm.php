ad5f6d4ddd7de14974dd1f8f108a96468e354192
will5810 <79901583+will5810@users.noreply.github.com>
2021-07-02 16:47:42 +0900

    Update loginForm.php

--- web/view/loginForm.php
+++ web/view/loginForm.php
@@ -1,3 +1,4 @@
+<!--사용자 로그인 페이지 입니다.-->
 <?php
     if($_SERVER['REQUEST_METHOD']=='POST' && $this->action=='login'){
         print "<script>alert('".$this->data."');</script>";
@@ -8,23 +9,23 @@
   <head>
     <meta charset="utf-8">
     <title>iamgorund</title>
-    <link rel="stylesheet" href="./source/iamground.css">     
+    <link rel="stylesheet" href="./source/iamground.css">
   </head>
   <body>
   <div class='container'>
-    <form method="post" action="./index.php?action=login"> 
-      <table id="loginTable">
+    <form method="post" action="./index.php?action=login">
+      <table class="iamgroundTable">
         <tr>
           <td>아이디 :</td>
-          <td><input type="text" name="uid"/></td>
-          <td rowspan=2><input class="btnLogin" type="submit" value="sign in" /></td>
+          <td><input type="text" name="userId" autocomplete="off" /></td>
+          <td rowspan=2><input class="btnLogin" type="submit" value="sign in"/></td>
         </tr>
         <tr>
           <td>비밀번호 : </td>
-          <td><input type="password" name="pwd" /></td>
+          <td><input type="password" name="userPw" /></td>
         </tr>
-      </table>      
-      </form>    
+      </table>
+      </form>
   </div>
   </body>
 </html>
