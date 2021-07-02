3b0f08608c7cf2a187ee6432fc9d6a85bd85aba8
will5810 <79901583+will5810@users.noreply.github.com>
2021-07-02 16:37:28 +0900

    Update MapDAO.php

--- web/model/MapDAO.php
+++ web/model/MapDAO.php
@@ -1,10 +1,8 @@
 <?php
-    //echo "<script> console.log('mapdao load'); </script>";
     require_once 'MapDTO.php';
     require_once 'DBConnector.php';
 
     class MapDAO{
-        //private static $instance = new MapDAO();
         private static $instance = null;
         private $connection = null;
 
@@ -14,13 +12,13 @@
         }
 
         public static function getInstance(){
-           // echo "<script> console.log('dao getInstance'); </script>";
             if(self::$instance == null){
                 self::$instance = new self;
             }
             return self::$instance;
         }
 
+        //유져아이디로 맵정보를 가져온다.
         public function mSelectMapByUserId($userId){
             $mapDTOs = array();
             $sql = "SELECT * FROM map WHERE user_id=?";
@@ -36,38 +34,24 @@
             }
             return $userDTOs;
         }
-        /*
-        public function mSelectMapMoByUserId($userId){
-            $dtoArr = array();
-            $sql = "SELECT m.map_id , m.map_location, mo.mo_id, mo.mo_type
-            FROM map AS m JOIN mobile_object AS mo ON m.map_id = mo.map_id WHERE m.user_id = ?";
-            $stmt = $this->connection->prepare($sql);
-            $stmt->bind_param('s', $userId);
-            $stmt->execute();
-            $stmt->bind_result($mid, $mlocation, $moid, $motype);
-            while($stmt->fetch()){
-                $mapDTO = new MapDTO($mid, null, $mlocation);
-                $moDTO = new MoDTO($moid, null, null, $motype);
-                array_push($dtoArr, $mapDTO);
-                array_push($dtoArr, $moDTO);
-            }
-            return $dtoArr;
-        }
-        */
 
+        //맵 정보를 추가한다.
         public function insertMap($mapDTO){
             $sql = "INSERT INTO map VALUES (?, ?, ?)";
             $stm = $this->connection->prepare($sql);
             $stm->bind_param('sss', $mapDTO->getMapId(), $mapDTO->getUserId() ,$mapDTO->getMapLocation());
             $stm->execute();
-
         }
+
+        //맵 정보를 수정한다.
         public function mUpdateMap($mapDTO){
             $sql = "UPDATE map SET map_location=? WHERE map_id=?";
             $stm = $this->connection->prepare ($sql);
             $stm->bind_param('ss', $mapDTO->getMapLocation(), $mapDTO->getMapId());
             $stm->execute();
         }
+
+        //맵 정보를 삭제한다.
         public function mDeleteMap($mapId){
             $sql = "delete from map where map_id=?;";
             $stm = $this->connection->prepare ($sql);
@@ -75,21 +59,22 @@
             $stm->execute();
         }
 
+        //오브젝트 아이디로 맵과 오브젝트 정보를 가져온다.
         public function mMonitoringByMoId($moId){
             $dtoArr = array();
-            $sql = "SELECT m.map_id, m.user_id, m.map_location, mo.mo_id, mo.mo_type FROM map AS m JOIN mobile_object AS mo ON m.map_Id = mo.map_Id WHERE mo.mo_id = ".$moId;
+            $sql = "SELECT m.map_id, m.user_id, m.map_location, mo.mo_id, mo.mo_type FROM map AS m JOIN mobile_object AS mo ON m.map_Id = mo.map_Id WHERE mo.mo_id = ? ";
             $stmt = $this->connection->prepare($sql);
-            //
-            //$stmt->bind_param('s', $moId);
+
+            $stmt->bind_param('s', $moId);
             $stmt->execute();
             $stmt->bind_result($mid, $uid, $location, $moid, $motype);
             if($stmt->fetch()){
                 $mapDTO = new MapDTO($mid, $uid, $location);
-                $moDTO = new MoDTO($moid, null, null, $motype, null);
+                $moDTO = new MoDTO($moid, null, null, $motype, null, null);
                 array_push($dtoArr, $mapDTO);
                 array_push($dtoArr, $moDTO);
             }
-        return $dtoArr;
+            return $dtoArr;
         }
         /*
         public function mInsertMapImage($mapImage){
