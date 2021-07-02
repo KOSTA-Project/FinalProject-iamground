c7b93c165d40aa45ca6ba4af282a6a9459540a30
will5810 <79901583+will5810@users.noreply.github.com>
2021-07-02 16:31:50 +0900

    Update MapController.php

--- web/controller/MapController.php
+++ web/controller/MapController.php
@@ -1,24 +1,26 @@
 <?php
     //echo "<script> console.log('mapcontroller load'); </script>";
     require_once './model/MapDAO.php';
-    class MapController{        
+    class MapController{
         private $mapdao;
         private $view;
 
+        //생성자 , MapDAO 객체를 변수에 대입한다.
         public function __construct(){
             $this->mapdao = MapDAO::getInstance();
         }
 
+        //각 메소드의 실행결과를 바탕으로 프론트컨틀롤에  controlView() 메소드에 전달한다.
         public function returnView($responseData=null){
             $frontController = new FrontController();
             $frontController->controlView($this->view, $responseData);
         }
 
+        //유저아이디로 맵정보를 조회한다.
         public function cSelectMapByUserId($userId){
             $mapDTOs = $this->mapdao->mSelectMapByUserId($userId);
-            
             if($mapDTOs==null){                     // map not exist
-                $this->view = 'notExistMap.php';      // check
+                $this->view = 'mapList.php';
                 $this->returnView();
             } else {                                // map exist
                 $this->view = 'mapList.php';
@@ -26,10 +28,34 @@
             }
         }
 
-        public function cMonitoringByMoId($moId){            
+        //오브젝트아디로 맵과 오브젝트정보 조회ㅇ
+        public function cMonitoringByMoId($moId){
             $dtoArr = $this->mapdao->mMonitoringByMoId($moId);
             $this->view = 'monitoring.php';
             $this->returnView($dtoArr);
+        }
+
+        //맵 정보 추가
+        public function cAddMap($mapId, $userId, $mapLocation){
+            $mapDTO = new MapDTO($mapId, $userId, $mapLocation);
+            $this->mapdao->insertMap($mapDTO);
+            $frontController = new FrontController('action=userInfo');
+            $frontController->run();
+        }
+
+        //맵 정보 수정
+        public function cUpdateMap($mapId, $mapLocation){
+            $mapDTO = new MapDTO($mapId, null, $mapLocation);
+            $this->mapdao->mUpdateMap($mapDTO);
+            $frontController = new FrontController('action=userInfo');
+            $frontController->run();
+        }
+
+        //맵 정보 삭제
+        public function cDeleteMap($mapId){
+            $this->mapdao->mDeleteMap($mapId);
+            $frontController = new FrontController('action=userInfo');
+            $frontController->run();
         }
 
         /*
@@ -44,30 +70,6 @@
             $this->dao->mInsertMapImage($file);
         }
         */
-
-        public function cAddMap($mapId, $userId, $mapLocation){
-            $mapDTO = new MapDTO($mapId, $userId, $mapLocation);
-            echo "<script> console.log('function cAddMap'); </script>";
-            $this->mapdao->insertMap($mapDTO);
-            $frontController = new FrontController('action=userInfo');
-            $frontController->run();	
-        }
-
-        public function cUpdateMap($mapId, $mapLocation){
-            $mapDTO = new MapDTO($mapId, null, $mapLocation);
-            echo "<script> console.log('function cUpdateMap'); </script>";
-            $this->mapdao->mUpdateMap($mapDTO);
-            $frontController = new FrontController('action=userInfo');
-            $frontController->run();	
-        } 
-
-        public function cDeleteMap($mapId){
-            echo "<script> console.log('function cDeleteMo'); </script>";
-            $moDTO = new MoDTO($userId , $mapId, $moId, $moType);
-            $this->mapdao->mDeleteMap($mapId);
-            $frontController = new FrontController('action=userInfo');
-            $frontController->run();
-        }
 
     }
 
