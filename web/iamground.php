24d6add45324039d0743547167e7ff428bd5ec3b
will5810 <79901583+will5810@users.noreply.github.com>
2021-07-02 16:35:21 +0900

    Update iamground.php

--- web/iamground.php
+++ web/iamground.php
@@ -1,14 +1,17 @@
+<!-----------------------------------------------
+    This is main gate of Iamground Web Service
+------------------------------------------------>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">    
+    <meta charset="UTF-8">
     <title>Iamground</title>
     <link rel="stylesheet" href="./source/iamground.css">
 </head>
 <body>
     <div class='container'>
         <h1 class='logo'><span class='logoI'>I</span><span class='logoAM'>AM</span><span class='logoGround'>Ground</span></h1>
-        <button onclick="window.location.href='./index.php?action=loginForm'">START</button>        
+        <button onclick="window.location.href='./index.php?action=loginForm'">START</button>
     </div>
 </body>
 </html>
