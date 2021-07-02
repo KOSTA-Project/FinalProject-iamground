cfaaa23b6c182abbf6e884f5b1402b69c10e6ffe
will5810 <79901583+will5810@users.noreply.github.com>
2021-07-02 16:54:53 +0900

    Update userManageForm.php

--- web/view/userManageForm.php
+++ web/view/userManageForm.php
@@ -1,3 +1,4 @@
+<!--회원을 관리합니다.-->
 <!DOCTYPE html>
 <html>
   <head>
@@ -7,22 +8,37 @@
     <?php
         $userDTO= $this->responseData;
     ?>
-
     </head>
     <body>
         <div class ="container">
             <h3>사용자 관리</h3>
-            <h4> 모든 사용자 아이디</h4>
-            <? for($i = 0; $i < count($userDTO); $i++) :?>
-                <div><?php echo $i+1,"."?> <?php if($userDTO[$i]->getUserType()=='2') $i+=1; ?> <?php echo $userDTO[$i]->getUserId();?></div>
-            <? endfor; ?>
-            <form method="POST" action="../am/index.php?action=userInfo">
-                <h4>조회 및 수정</h4>
-                    <div>아이디 : <input type="text" name="userId" autocomplete="off"/></div>
-                    <input type="submit" class="btnManage" value ="검색"/>
-                </form>
+            <table class="iamgroundTable alineRight">
+                <tr>
+                            <td>
+                        <form method="POST" action="../am/index.php?action=userInfo">
+                            <input type="text" name="userId" autocomplete="off"/>
+                            <input type="submit" class="btnManage" value ="검색"/>
+                        </form>
+                    </td>
+                </tr>
+            </table>
+            <table class="iamgroundTable">
+                <h4>모든 사용자 아이디</h4>
+                <?php $num=0; ?>
+                <? for($i = 0; $i < count($userDTO); $i++) :?>
+                   <? if($userDTO[$i]->getUserType()==2) {continue;}?>
+                    <tr>
+                        <form method="POST" action="../am/index.php?action=userInfo">
+                            <input type="hidden" name="userId" value="<?php echo $userDTO[$i]->getUserId();?>"/>
+                            <td class="alineLeft"><?php echo ++$num,"."?></td>
+                            <td class="alineRight"><?php echo $userDTO[$i]->getUserId();?></td>
+                            <td class="alineRight"><input type="submit" class="btnManage" value ="선택"/></td>
+                        </form>
+                    </tr>
+                <? endfor; ?>
+            </table>
             <form method="POST" action="../am/index.php?action=manager">
-                <input type="submit" class="btnManage" value ="돌아가기"/>
+                <input type="submit" class="btnManage width200px" value ="돌아가기"/>
             </form>
         </div>
 </body>
