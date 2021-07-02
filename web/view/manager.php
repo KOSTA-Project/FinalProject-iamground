a9ebad2c16b16f891083d7ce1ff659d99e2056e0
will5810 <79901583+will5810@users.noreply.github.com>
2021-07-02 16:48:09 +0900

    Update manager.php

--- web/view/manager.php
+++ web/view/manager.php
@@ -1,22 +1,36 @@
-<!DOCTYPE html>
-<html>
-<?php
-header("Cache-Control: no cache");
-?>
+<!--관리자 페이지 입니다.--> <!DOCTYPE html> <html> <?php header("Cache-Control: no cache"); ?>
   <head>
     <meta charset="utf-8">
     <title>Manager</title>
     <link rel="stylesheet" href="./source/iamground.css">
   </head>
   <body>
+    <h3>관리자 페이지</h3>
     <div class='container'>
       <h3>회원 관리</h3>
-      <form method="POST" action="./index.php?action=userManage">
-        <input type="submit" class="btnManage" value ="사용자 관리"/>
-      </form>
-      <form method="POST" action="./index.php?action=joinForm">
-        <input type="submit" class="btnManage" value ="사용자 추가"/> </td>
-      </form>
+      <table class="iamgroundTable">
+        <tr>
+          <td>
+            <form method="POST" action="./index.php?action=userManage">
+              <input type="submit" class="btnManage width200px" value ="사용자 관리"/>
+            </form>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <form method="POST" action="./index.php?action=joinForm">
+              <input type="submit" class="btnManage width200px" value ="사용자 추가"/> </td>
+            </form>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <form method="POST" action="./index.php?action=logout">
+              <input type="submit" class="btnManage width200px" value ="관리자 로그아웃"/> </td>
+            </form>
+          </td>
+        </tr>
+      </table>
     </div>
   </body>
 </html>
