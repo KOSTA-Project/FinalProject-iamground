9deb85e4aa267b4eae67f2d8bc017e8e410b0c99
will5810 <79901583+will5810@users.noreply.github.com>
2021-07-02 16:44:50 +0900

    Update addMapForm.php

--- web/view/addMapForm.php
+++ web/view/addMapForm.php
@@ -1,29 +1,35 @@
+<!--맵을 추가 합니다.-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
     <link rel="stylesheet" href="./source/iamground.css">
+
 </head>
 <body>
     <div class='container'>
     <h3>Add Map</h3>
     <form method="POST" action="../am/index.php?action=addMap">
     <input type="hidden" name="userId" value="<? echo $_POST['userId']?>"/>
-    <table class="manageTable">
+    <table class="iamgroundTable">
         <tr>
-            <td>map id:</td>
+            <td class="alineRight">map id:</td>
             <td><input type="text" name="mapId" autocomplete="off"/></td>
         </tr>
         <tr>
-            <td>map Location:</td>
+            <td class="alineRight">map Location:</td>
             <td><input type="text" name="mapLocation" autocomplete="off"/></td>
         </tr>
         <tr>
-            <td><input type="submit" value="추가" autocomplete="off"/></td>
+            <td colspan="2" class="alineCenter"><input class="btnManage width200px" type="submit" value="추가" autocomplete="off"/></td>
         </tr>
     </table>
     </form>
+            <form method="POST" action="../am/index.php?action=userInfo">
+                <input type="hidden" name="userId" value="<? echo $_POST['userId']?>"/>
+                <input type="submit" class="btnManage width200px" value ="돌아가기"/>
+            </form>
     </div>
 </body>
 </html>
