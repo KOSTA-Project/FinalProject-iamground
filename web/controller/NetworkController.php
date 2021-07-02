f6c3fdf1a8873c1a8ffee8a507799c4699bca27b
will5810 <79901583+will5810@users.noreply.github.com>
2021-07-02 16:33:59 +0900

    Update NetworkController.php

--- web/controller/NetworkController.php
+++ web/controller/NetworkController.php
@@ -1,34 +1,35 @@
-<?php    
+<?php
+    //direction 정보가 있으면 sendKeyToMO()를 호출한다.
     if(isset($_POST["direction"])){
         sendKeyToMO();
     }
 
-    // socket connect and send key word which get from controlByWSAD.js
+    //controlByWSAD.js에서 전달받은 키워드를 소켓을 통해 전달한다.
     function sendKeyToMO(){
         $output = $_POST["direction"];
-        $address = $_POST["address"];     // hard coding need to change
+        $address = $_POST["address"];
         echo $address;
         $port = 9001;
-        try{   
-            //echo $output;            
+        try{
+            //tcp소켓을 생성한다.
             $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
             if($socket === false){
                 echo "socket_create() failed: reason: ".socket_strerror(socket_last_error())."\n";
             }
-            
-            // try connect to raspberry pi server
+            // 오브젝트 서버에 접속을 시도한다.
             $result = socket_connect($socket, $address, $port);
-            
+
             if($result === false){
                 echo "socket_connect() failed.\nReason: ($result) ". socket_strerror(socket_last_error($socket))."\n";
             }
             // delay 0.005 seconds
-            usleep(5000);   
-            // send message                 
-            socket_write($socket, $output, strlen($output));    
-            echo $output;    
+            usleep(5000);
+            // 메세지 전송한다.
+            socket_write($socket, $output, strlen($output));
+            echo $output;
         }finally{
-            socket_close($socket);                
+            //소켓을 닫는다.
+            socket_close($socket);
         }
     }
 ?>
