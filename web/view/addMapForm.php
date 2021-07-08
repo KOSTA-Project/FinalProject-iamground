9a3ea290875b86ff32f292be88e0ea36edcc2edb
JIHUN KANG <84777431+Ji-Hun-Kang@users.noreply.github.com>
2021-07-08 14:44:44 +0900

    210708 update kosta final project
    - add map form

--- web/view/addMapForm.php
+++ web/view/addMapForm.php
@@ -9,25 +9,25 @@
 </head>
 <body>
     <div class='container'>
-    <h3>Add Map</h3>
+    <h3>사업장 추가</h3>
     <form method="POST" action="../am/index.php?action=addMap">
     <input type="hidden" name="userId" value="<? echo $_POST['userId']?>"/>
     <table class="iamgroundTable">
         <tr>
-            <td class="alineRight">map id:</td>
+            <td class="alineRight">아이디:</td>
             <td><input type="text" name="mapId" autocomplete="off"/></td>
         </tr>
         <tr>
-            <td class="alineRight">map Location:</td>
+            <td class="alineRight">위치:</td>
             <td><input type="text" name="mapLocation" autocomplete="off"/></td>
         </tr>
         <tr>
-            <td colspan="2" class="alineCenter"><input class="btnManage width200px" type="submit" value="추가" autocomplete="off"/></td>
+            <td colspan="2" class="alineCenter"><input class="btnManage width200px" type="submit" value="추가" autocomplete="off"/></td> 
         </tr>
     </table>
     </form>
             <form method="POST" action="../am/index.php?action=userInfo">
-                <input type="hidden" name="userId" value="<? echo $_POST['userId']?>"/>
+		<input type="hidden" name="userId" value="<? echo $_POST['userId']?>"/>
                 <input type="submit" class="btnManage width200px" value ="돌아가기"/>
             </form>
     </div>
