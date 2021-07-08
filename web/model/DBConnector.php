fe8c165d4bf126d6593509d3cd1bf41986556110
JIHUN KANG <84777431+Ji-Hun-Kang@users.noreply.github.com>
2021-07-08 14:38:02 +0900

    210708 update kosta final project
    - db connector

--- web/model/DBConnector.php
+++ web/model/DBConnector.php
@@ -1,11 +1,11 @@
 <?php
     class DBConnector{
-        // php인 경우 클래스 로딩시 생성자를 호출할 수 없다.
+	// php인 경우 클래스 로딩시 생성자를 호출할 수 없다.
         //private static $instance = new DBConnector();
         private static $instance = null;
         private $connection = null;
 
-        //객체 생성시 DB 접속한다.
+	//객체 생성시 DB 접속한다.
         private function __construct(){
             $this->connection = new mysqli('localhost', 'iamground', 'iamground', 'DB_iamground');
             if($connection->connect_error){
