fedccde3bae2c87c2be35dc07c07a585c545cbba
JIHUN KANG <84777431+Ji-Hun-Kang@users.noreply.github.com>
2021-07-08 14:47:11 +0900

    Update joinForm.php
    - 210708 update kosta final project

--- web/view/joinForm.php
+++ web/view/joinForm.php
@@ -11,22 +11,25 @@
             <div>
                 <h3>회원 생성</h3>
                  <form method="POST" action="./index.php?action=join">
-                    <table class="manageTable">
-                        <tr>
-                            <td>아이디 </td> <td><input type="text" name="userId" autocomplete="off" /></td>
-                        </tr>
-                        <tr>
-                            <td>비밀번호 </td> <td><input type="text" name="userPw" autocomplete="off"/></td>
-                        </tr>
-                        <tr>
-                            <td>권한 </td> <td><input type="text" name="userType" autocomplete="off"/></td>
-                        </tr>
-                        <tr>
-                            <td colspan="2"><input type="submit" class="btnManage" value ="확인"/></td>
-                        </tr>
+    		<table class="iamgroundTable">
+        	<tr>
+            		<td class="alineRight">아이디:</td>
+            		<td><input type="text" name="userId" autocomplete="off"/></td>
+       		 </tr>
+        	<tr>
+            		<td class="alineRight">비밀번호:</td>
+            		<td><input type="text" name="userPw" autocomplete="off"/></td>
+        	</tr>
+        	<tr>
+            		<td class="alineRight">권한:</td>
+            		<td><input type="text" name="userType" autocomplete="off"/></td>
+        	</tr>
+        	<tr>
+            		<td colspan="2" class="alineCenter"><input class="btnManage width200px" type="submit" value="추가" autocomplete="off"/></td>
+        	</tr>
                     </table>
                 </form>
-                    <form method="POST" action="../am/index.php?action=manager">
+	            <form method="POST" action="../am/index.php?action=manager">
                     <input type="submit" class="btnManage" value ="돌아가기"/>
                 </form>
            </div>
