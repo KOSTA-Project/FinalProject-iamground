cbc87080e521e0d6e8dcbd3e376a858d5b653ca9
JIHUN KANG <84777431+Ji-Hun-Kang@users.noreply.github.com>
2021-07-08 14:42:25 +0900

    210708 update kosta final project
    - control by wasd

--- web/source/controlByWSAD.js
+++ web/source/controlByWSAD.js
@@ -2,22 +2,22 @@
 document.addEventListener("DOMContentLoaded", function(){
     // get element object by id
     var log = document.querySelector('#log');
-
+     
 
     // set keypress event and function
     document.addEventListener('keypress', sendKeyToPhp);
-    document.addEventListener('keyup', sendXToPhp);
+    document.addEventListener('keyup', sendXToPhp);  
 });
 
 function isWASD(e){
     var wasdCheck = true;
-
+    
     if(e.key!=='w' && e.key!=='s' && e.key!=='a' && e.key!=='d'){
         alert("please press key between 'w', 's', 'a', 'd'");
-        wasdCheck = false;
+        wasdCheck = false;            
     } else {
         wasdCheck = true;
-    }
+    }    
     return wasdCheck;
 }
 
@@ -30,30 +30,30 @@ function sendWithAJAX(key){
         if(httpRequest.readyState==4 && httpRequest.status == 200){
             // get response data to server (is it null now)
             // var response = JSON.parse(httpRequest.responseText);
-            log.textContent = "press button"+httpRequest.responseText;
-        }
+            log.textContent = "press button"+httpRequest.responseText;                
+        } 
     };
 
     httpRequest.open('POST', './controller/NetworkController.php');
     httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     httpRequest.send('direction=' + key+ '&address='+'192.168.0.21');
-}
+}    
 
 // when key wsad pressed send key word to MO
 function sendKeyToPhp(e){
     if(isWASD(e)===false){
         return;
-    }
-
+    }      
+    
     sendWithAJAX(e.key);
-    changeBtnColor(e.key, 'o');
+    changeBtnColor(e.key, 'o');        
 }
 
 // when key wsad up send key x to MO
 function sendXToPhp(e){
     if(isWASD(e)===false){
         return;
-    }
+    }  
 
     sendWithAJAX('x');
     changeBtnColor(e.key, 'x');
@@ -61,11 +61,11 @@ function sendXToPhp(e){
 
 function changeBtnColor(e, ox){
     if(ox === 'x'){
-        var btnColor = "grey";
+        var btnColor = "grey"; 
     } else if(ox === 'o'){
         var btnColor = "red";
     }
-    document.getElementById(e).style.backgroundColor = btnColor;
+    document.getElementById(e).style.backgroundColor = btnColor;      
 }
 
 function sendKeyByMouse(btnValue){
@@ -78,3 +78,4 @@ function sendXByMouse(btnValue){
     changeBtnColor(btnValue, 'x');
 
 }
+
