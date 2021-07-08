fd9383a8469b55730f4d0b7051a5c7f87f58eb90
JIHUN KANG <84777431+Ji-Hun-Kang@users.noreply.github.com>
2021-07-08 14:31:57 +0900

    210708 update kosta final project
    - map controller

--- web/controller/MapController.php
+++ web/controller/MapController.php
@@ -5,18 +5,18 @@
         private $mapdao;
         private $view;
 
-        //생성자 , MapDAO 객체를 변수에 대입한다.
+	//생성자 , MapDAO 객체를 변수에 대입한다.
         public function __construct(){
             $this->mapdao = MapDAO::getInstance();
         }
 
-        //각 메소드의 실행결과를 바탕으로 프론트컨틀롤에  controlView() 메소드에 전달한다.
+	//각 메소드의 실행결과를 바탕으로 프론트컨틀롤에  controlView() 메소드에 전달한다.
         public function returnView($responseData=null){
             $frontController = new FrontController();
             $frontController->controlView($this->view, $responseData);
         }
 
-        //유저아이디로 맵정보를 조회한다.
+	//유저아이디로 맵정보를 조회한다.
         public function cSelectMapByUserId($userId){
             $mapDTOs = $this->mapdao->mSelectMapByUserId($userId);
             if($mapDTOs==null){                     // map not exist
@@ -28,14 +28,14 @@
             }
         }
 
-        //오브젝트아디로 맵과 오브젝트정보 조회ㅇ
+	//오브젝트아디로 맵과 오브젝트정보 조회ㅇ
         public function cMonitoringByMoId($moId){
             $dtoArr = $this->mapdao->mMonitoringByMoId($moId);
             $this->view = 'monitoring.php';
             $this->returnView($dtoArr);
         }
 
-        //맵 정보 추가
+	//맵 정보 추가
         public function cAddMap($mapId, $userId, $mapLocation){
             $mapDTO = new MapDTO($mapId, $userId, $mapLocation);
             $this->mapdao->insertMap($mapDTO);
@@ -43,7 +43,7 @@
             $frontController->run();
         }
 
-        //맵 정보 수정
+	//맵 정보 수정
         public function cUpdateMap($mapId, $mapLocation){
             $mapDTO = new MapDTO($mapId, null, $mapLocation);
             $this->mapdao->mUpdateMap($mapDTO);
@@ -51,7 +51,7 @@
             $frontController->run();
         }
 
-        //맵 정보 삭제
+	//맵 정보 삭제
         public function cDeleteMap($mapId){
             $this->mapdao->mDeleteMap($mapId);
             $frontController = new FrontController('action=userInfo');
