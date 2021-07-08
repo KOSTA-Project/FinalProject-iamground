731d97d17de72b3c98bdb00f2670118ba50601e9
JIHUN KANG <84777431+Ji-Hun-Kang@users.noreply.github.com>
2021-07-08 14:41:07 +0900

    210708 update kosta final project
    - user dao

--- web/model/UserDAO.php
+++ web/model/UserDAO.php
@@ -1,3 +1,4 @@
+
 <?php
     require_once 'UserDTO.php';
     require_once 'MapDTO.php';
@@ -21,7 +22,7 @@
             return self::$instance;
         }
 
-        //유저아이디로 유저정보를 가져온다.
+	//유저아이디로 유저정보를 가져온다.
         public function selectById($userId){
             $userDTO = null;
             $sql = "SELECT * FROM user WHERE user_id=?";
@@ -35,8 +36,8 @@
             return $userDTO;
         }
 
-        //모든 유저정보를 가져온다.
-        public function selectAllId(){
+	//모든 유저정보를 가져온다.
+     	public function selectAllId(){
             $dtoUser = array();
             $sql = "SELECT * FROM user";
             $stmt = $this->connection->prepare($sql);
@@ -44,12 +45,12 @@
             $stmt->bind_result($id, $pw, $type);
             while($stmt->fetch()){
                 $userDTO = new UserDTO($id, $pw, $type);
-                array_push($dtoUser,$userDTO);
+		array_push($dtoUser,$userDTO);
             }
             return $dtoUser;
         }
 
-        //유저정보를 추가한다.
+	//유저정보를 추가한다.
         public function InsertUser($userDTO){
             $sql = "INSERT INTO user VALUES(?,?,?)";
             $stm = $this->connection->prepare($sql);
@@ -57,7 +58,7 @@
             $stm->execute();
         }
 
-        //유저정보를 수정한다.
+	//유저정보를 수정한다.
         public function mUpdateUser($userDTO){
             echo "<script> console.log('update start'); </script>";
             $sql = "update user set  user_pwd=?, user_type=? where user_id=?";
@@ -66,7 +67,7 @@
             $stm->execute();
         }
 
-        //유저정보를 삭제한다.
+	//유저정보를 삭제한다.
         public function mDeleteUser($userId){
             $sql = "delete from user where user_id=?;";
             $stm = $this->connection->prepare ($sql);
